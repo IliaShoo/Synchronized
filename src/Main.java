aa566f0b402ca6affd6a1d95808605227c85c9c4
Ilia Shudegov <faun-08@mail.ru>
2023-02-23 10:00:12 +0300

    without magic

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final String LETTER = "RLRFR";
+    public static final int LENGTH = 100;
+    public static final int AMOUNT_OF_THREADS = 1000;
 
     public static void main(String[] args) throws InterruptedException {
 
         List<Thread> myThreads = new ArrayList<>();
 
         Runnable runnable = () -> {
-            String result = generateRoute("RLRFR", 100);
+            String result = generateRoute(LETTER, LENGTH);
 
             //подсчет R в строке
             int amountOfRLetter = 0;
@@ -27,7 +30,7 @@ public class Main {
             }
         };
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < AMOUNT_OF_THREADS; i++) {
             myThreads.add(new Thread(runnable));
             myThreads.get(i).start();
         }
